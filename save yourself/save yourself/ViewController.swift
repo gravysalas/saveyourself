c31316828958b4dc1a74afed136b3409b6842b2a
Eric Salas <eric@saturdaysgravy.com>
2017-08-22 22:20:17 -0500

    added a variable

--- save yourself/save yourself/ViewController.swift
+++ save yourself/save yourself/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let eric = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
